55ab9eb677085d49931e3c73e2640fd414f831a3
Владислав <40837008+Wladislaw28@users.noreply.github.com>
2019-11-07 14:59:54 +0300

    add-project

---
+++ Assets/Move.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public GameObject player;
+    public int speedRotation = 1;
+    public int speed = 2;
+    //public AnimationClip anima;
+    public int jumpSpeed = 150;
+
+    // Start is called before the first frame update
+    void Start () {
+        player = (GameObject) this.gameObject;
+        // GetComponent<Animation> ().AddClip (anima, "animCube");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) {
+            player.transform.position -= player.transform.up * speed * Time.deltaTime;
+            //GetComponent<Animation> ().CrossFade ("animCube");
+        }
+        if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) {
+            player.transform.position += player.transform.up * speed * Time.deltaTime;
+            //player.transform.Rotate (Vector3.down * speedRotation);
+        }
+        if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+            player.transform.position -= player.transform.right * speedRotation * Time.deltaTime;
+            //player.transform.Rotate (Vector3.left * speedRotation);
+        }
+        if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
+            player.transform.position += player.transform.right * speedRotation * Time.deltaTime;
+            //player.transform.Rotate (Vector3.right * speedRotation);
+        }
+        if (Input.GetKeyDown (KeyCode.Space)) {
+            player.transform.position += player.transform.forward * jumpSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/move1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move1 : MonoBehaviour
+{
+     
+        public GameObject player;
+        public int speedRotation = 3;
+        public int speed = 5;
+        public AnimationClip anima;
+        public int jumpSpeed = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
